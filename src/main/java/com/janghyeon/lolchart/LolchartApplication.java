83c0b6ef7de1e23d5707dabd66b6567733186ffa
2xold <yapiiens@gmail.com>
2023-11-21 22:44:20 +0900

    first

---
+++ src/main/java/com/janghyeon/lolchart/LolchartApplication.java
@@ -0,0 +1,13 @@
+package com.janghyeon.lolchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolchartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janghyeon/lolchart/LolchartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janghyeon.lolchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolchartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
